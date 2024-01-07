6f471295115db890e9a5022fe2cacb1a13a99d38
Fabian Wunsch <fabian@uriah.heep.sax.de>
2024-01-07 11:09:03 +0100

    fix offset

--- breakwater-parser/src/implementations/simple.rs
+++ breakwater-parser/src/implementations/simple.rs
@@ -141,7 +141,7 @@ impl Parser for SimpleParser {
                         continue;
                     }
                 }
-            } else if current_command & 0x0000_ffff_ffff_ffff == string_to_number(b"OFFSET \0\0") {
+            } else if current_command & 0x00ff_ffff_ffff_ffff == string_to_number(b"OFFSET \0\0") {
                 i += 7;
 
                 let (x, y, present) = parse_pixel_coordinates(buffer.as_ptr(), &mut i);
